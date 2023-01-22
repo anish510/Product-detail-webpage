51f7f95d080605c7d659ce6b04931fcad430731d
anish510 <anishjoshi510@gmail.com>
2023-01-22 22:59:50 +0545

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -23,8 +23,23 @@ function App() {
         <div className={classes.ProductData}>
           <h1 className={classes.ProductTitle}>{ProductData.title} </h1>
           <p className = {classes.ProductDescription}>{ProductData.description}</p><br/>
-          <h3>Select Color</h3>
-          
+          <h3 className={classes.SectionHeading}>Select Color</h3>
+          <div className={classes.ProductImageContainer}>
+            <img className={classes.ProductImage} src = 'https://imgur.com/PTgQlim.png' alt='Red Colored Watch'/>
+            <img className={classes.ProductImage} src = 'https://imgur.com/Mplj1YR.png' alt='Blue Colored Watch'/>
+            <img className={classes.ProductImage} src = 'https://imgur.com/xSIK4M8.png' alt='Purple Colored Watch'/> 
+            <img className={[classes.ProductImage , classes.SelectedProductImage].join(' ')} src = 'https://imgur.com/iOeUBV7.png' alt='Black Colored Watch'/> 
+          </div>
+          <br/>
+
+          <h3 className={classes.SectionHeading}>Features</h3>
+          <div>
+            <button className={[classes.FeatureItem, classes.SelectedFeatureItem].join(' ')}>Time</button>
+            <button className={classes.FeatureItem}>Heart Rate</button>
+
+          </div>
+
+          <button className={classes.PrimaryButton}>Buy Now</button>
         </div>
 
       </div>
